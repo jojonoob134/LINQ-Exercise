cfa47727462992c76db87ac3a982c7d5f4ac34f5
Jorge Saldana <jojonoob134@gmail.com>
2023-09-13 11:19:16 -0500

    did the exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameStuff = new List<string>();
+            gameStuff.Add("hollow kight");
+            gameStuff.Add("GTA 5");
+            gameStuff.Add("Librery of ruina");
+            gameStuff.Add("balders gate 3");
+            gameStuff.Add("persona 5 royal");
+            gameStuff.Add("elden ring");
+            
+            var orderGames  = gameStuff.OrderBy(x => x.Length);
+
+            foreach (var game in orderGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
